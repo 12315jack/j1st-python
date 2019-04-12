6499cac0e0f831357408e91888e6a0ecfed74032
12315jack <chenyj_work@163.com>
2019-04-12 16:09:40 +0800

    add basic data type doc

---
+++ python-basic/codeFiles/basicDataType.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+print ("==============hello world=================")
+
+#基础数据类型
+#数字  ---> int类
+
+s = '123'
+i = int( s )
+print( i)
+
+#布尔值  --->bool类
+
+
+#字符串  --->str类
+
+
+#列表  --->list类
+
+
+#元组  --->tuple类
+
+
+#字典  --->dict类
+
+
