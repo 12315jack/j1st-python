73e7da57ebd8d4550bee197b9cce3c29c3192687
12315jack <chenyj_work@163.com>
2019-05-06 19:57:21 +0800

    add basic data type doc

--- python-basic/code_files/basicDataType.py
+++ python-basic/code_files/basicDataType.py
@@ -4,25 +4,32 @@
 
 print ("==============hello world=================")
 
-#基础数据类型
-#数字  ---> int类
+# 基础数据类型
+# 数字  ---> int类
 
 s = '123'
 i = int( s )
 print( i)
 
-#布尔值  --->bool类
+a='你好世界'
+print(a)
+
+# 布尔值  --->bool类
+if True:
+    print("Answer")
+    print("True")
+else:
+    print("False")
+
+# 字符串  --->str类
 
 
-#字符串  --->str类
+# 列表  --->list类
 
 
-#列表  --->list类
+# 元组  --->tuple类
 
 
-#元组  --->tuple类
-
-
-#字典  --->dict类
+# 字典  --->dict类
 
 
