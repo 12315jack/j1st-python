1e8c36320953604a678dd92208b065388de923f8
12315jack <chenyj_work@163.com>
2019-12-10 16:56:39 +0800

    add new demo

---
+++ python-basic/code_files/basic/p_whileFor.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# while 循环
+count = 0
+while count < 5:
+    print(count, " 小于 5")
+    count = count + 1
+else:
+    print(count, " 大于或等于 5")
+
+# for循环
+sites = ["Baidu", "Google", "Runoob", "Taobao"]
+for site in sites:
+    if site == "Runoob":
+        print("菜鸟教程!")
+        break
+    print("循环数据 " + site)
+else:
+    print("没有循环数据!")
+print("完成循环!")
+
+# range()函数
+for i in range(5):
+    print(i)
+
+for i in range(5, 9):
+    print(i)
+
+for i in range(0, 10, 3):
+    print(i)
+
+a = ['Google', 'Baidu', 'Runoob', 'Taobao', 'QQ']
+for i in range(len(a)):
+    print(i, a[i])
+
+# break和continue语句及循环中的else子句
+for letter in 'Runoob':  # 第一个实例
+    if letter == 'o':
+        print('退出前的字母为 :', letter)
+        break
+    print('当前字母为 :', letter)
+
+var = 10  # 第二个实例
+while var > 0:
+    print('当期变量值为 :', var)
+    var = var - 1
+    if var == 5:
+        break
+
+print("Good bye!")
+
+# for循环也可以有else
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, '等于', x, '*', n//x)
+            break
+    else:
+        # 循环中没有找到元素
+        print(n, ' 是质数')
